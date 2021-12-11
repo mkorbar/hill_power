dbc6a2a3c1200c6eb30f2bb2fdc9fc6ed615fba6
Matej Korbar <matej.korbar@dlabs.si>
2021-12-11 22:49:45 +0100

    Add the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hill_power';
+
+  hillGrade: FormControl;
+  weight: FormControl;
+  cda: FormControl;
+  wheelRadius: FormControl;
+  torque: FormControl;
+
+  speed = 40 / 3.6;
+
+  row: any;
+
+  climb = 0;
+  power = 0;
+  aero_drag = 0;
+  roll_drag = 0;
+  total_power = 0;
+  rpm = 0;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.hillGrade = this.formBuilder.control(5);
+    this.weight = this.formBuilder.control(1450);
+    this.cda = this.formBuilder.control(0.6);
+    this.wheelRadius = this.formBuilder.control({value: 0.317, disabled: true} );
+    this.torque = this.formBuilder.control(140);
+
+    this.hillGrade.valueChanges.subscribe(() => {
+      this.recalculate();
+    });
+    this.weight.valueChanges.subscribe(() => {
+      this.recalculate();
+    });
+    this.cda.valueChanges.subscribe(() => {
+      this.recalculate();
+    });
+    this.wheelRadius.valueChanges.subscribe(() => {
+      this.recalculate();
+    });
+    this.torque.valueChanges.subscribe(() => {
+      this.recalculate();
+    });
+
+    this.recalculate();
+  }
+
+  recalculate() {
+    console.log('recalculating');
+    this.climb = this.speed * this.hillGrade.value / 100;
+    this.power = this.climb * this.weight.value * 9.81;
+    this.aero_drag = 0.5 * 1.225 * Math.pow(this.speed, 3) * this.cda.value;
+    this.roll_drag = 137 * this.speed;
+    this.total_power = this.power + this.aero_drag + this.roll_drag;
+    this.rpm = this.total_power / this.torque.value * 9.54;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule
   ],
   providers: [],
